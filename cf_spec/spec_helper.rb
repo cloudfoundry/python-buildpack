f16f29d29c589c6879a464e236b0c1afdc7cc3b0
Anna Thornton <athornton@pivotal.io>
2017-02-01 12:26:02 -0500

    Add timeout to integration tests and retry on timeout
    [#138839265]
    
    Signed-off-by: Dave Goddard <dave@goddard.id.au>
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- cf_spec/spec_helper.rb
+++ cf_spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'bundler/setup'
 require 'machete'
 require 'machete/matchers'
+require 'timeout'
 
 `mkdir -p log`
 Machete.logger = Machete::Logger.new("log/integration.log")
@@ -11,4 +12,19 @@ RSpec.configure do |config|
 
   config.filter_run_excluding :cached => ENV['BUILDPACK_MODE'] == 'uncached'
   config.filter_run_excluding :uncached => ENV['BUILDPACK_MODE'] == 'cached'
+
+  config.around(:each) do |example|
+    allowed_time = 20 * 60 # 20 minutes per attempt
+    begin
+      Timeout::timeout(allowed_time) do
+        example.run
+      end
+    rescue Timeout::Error
+      RSpec.configuration.reporter.message("Retry try #{example.location}")
+
+      Timeout::timeout(allowed_time) do
+        example.run
+      end
+    end
+  end
 end
