019a05a5da3da8fc7427cdf91041d0fca4a5129d
Tyler Phelan <tphelan@pivotal.io>
2018-03-29 14:33:21 -0400

    GOOS=linux when packaging
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ set -exuo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 source .envrc
 
-go build -ldflags="-s -w" -o bin/supply python/supply/cli
-go build -ldflags="-s -w" -o bin/finalize python/finalize/cli
+GOOS=linux go build -ldflags="-s -w" -o bin/supply python/supply/cli
+GOOS=linux go build -ldflags="-s -w" -o bin/finalize python/finalize/cli
