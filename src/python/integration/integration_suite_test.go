b9900ccbc931959a712b2bef56880a578ad16ef4
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2018-08-17 10:21:46 -0400

    Remove debugging printlns.
    Because they are unnecessary.

--- src/python/integration/integration_suite_test.go
+++ src/python/integration/integration_suite_test.go
@@ -148,21 +148,18 @@ func AssertNoInternetTraffic(fixtureName string) {
 			Skip("Running uncached tests")
 		}
 
-		fmt.Println("### Copying bp file")
 		bpFile := filepath.Join(bpDir, buildpackVersion+"tmp")
 		cmd := exec.Command("cp", packagedBuildpack.File, bpFile)
 		err := cmd.Run()
 		Expect(err).To(BeNil())
 		defer os.Remove(bpFile)
 
-		fmt.Println("### Running internet traffic test")
 		traffic, built, logs, err := cutlass.InternetTraffic(
 			bpDir,
 			filepath.Join("fixtures", fixtureName),
 			bpFile,
 			[]string{"LC_ALL C.UTF-8", "LANG C.UTF-8"},
 		)
-		fmt.Println("### Checking internet traffic results")
 		Expect(err).To(BeNil())
 		Expect(built).To(BeTrue(), strings.Join(logs, "\n"))
 		Expect(traffic).To(BeEmpty(), strings.Join(logs, "\n"))
