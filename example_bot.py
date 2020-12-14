459a11ab0fcce65f8f9d3cca644f059b07840579
Nunya-9018 <73594766+Nunya-9018@users.noreply.github.com>
2020-12-14 15:34:49 -0500

    Add files via upload

---
+++ example_bot.py
@@ -0,0 +1,19 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('NzgwNTMzMjkyNjE2Nzc3NzYw.X7weRg.-QbbWDxItoK0uAq64GEGP4xLpys')
