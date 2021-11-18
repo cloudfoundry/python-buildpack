f3a2bd10e55489e030ede5ab62a9e7a0104ec83b
Arjun Sreedharan <asreedharan@vmware.com>
2021-11-18 13:07:20 -0800

    Simplify miniconda_python_3 fixture
    Remove unused deps and code
    
    Signed-off-by: Andrew Meyer <meyera@vmware.com>

--- fixtures/miniconda_python_3/app.py
+++ fixtures/miniconda_python_3/app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import pytest
 import os
 import importlib
 import sys
@@ -17,25 +16,6 @@ for m in MODULE_NAMES:
 app = Flask(__name__)
 
 
-@app.route('/<module_name>')
-def in_module_tests(module_name):
-    if module_name not in modules:
-        return "This module is not listed"
-    try:
-        result = modules[module_name].test()
-        num_failures = result.failures
-        result_string = "{}: number of failures={}".format(module_name, len(num_failures))
-    except (NameError, ImportError, AttributeError):
-        result_string = "{}: Error running test!".format(module_name)
-    return result_string
-
-
-@app.route('/all')
-def run_all():
-    results = "<br>\n".join([in_module_tests(m) for m in MODULE_NAMES])
-    return str(results)
-
-
 def module_version(module_name):
     m = modules[module_name]
     if m is None:
@@ -47,14 +27,9 @@ def module_version(module_name):
 
 @app.route('/')
 def root():
-    versions = "<br>\n".join([module_version(m) for m in MODULE_NAMES])
-    python_version = "\npython-version%s\n" % sys.version
-    r = """<br><br>
-    Imports Successful!<br>
-
-    To test each module go to /numpy
-    or test all at /all.<br>
-    Test suites can take up to 10 minutes to run, main output is in app logs."""
+    versions = "<br>"+("<br>".join([module_version(m) for m in MODULE_NAMES]))
+    python_version = "python-version%s" % sys.version
+    r = "<br><br>Imports Successful!<br>"
     return python_version + versions + r
 
 if __name__ == '__main__':
