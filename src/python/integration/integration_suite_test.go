c281ce7f00f98f348a801c89e99b1b86d4726931
Ryan Moran <rmoran@vmware.com>
2022-08-23 10:48:39 -0700

    Adds --stack flag for integration tests

--- src/python/integration/integration_suite_test.go
+++ src/python/integration/integration_suite_test.go
@@ -25,6 +25,7 @@ var isSerialTest bool
 var packagedBuildpack cutlass.VersionedBuildpackPackage
 var token string
 var platform string
+var stack string
 
 var _ = func() bool {
 	testing.Init()
@@ -39,6 +40,7 @@ func init() {
 	flag.StringVar(&cutlass.DefaultDisk, "disk", "384M", "default disk for pushed apps")
 	flag.StringVar(&token, "github-token", "", "use the token to make GitHub API requests")
 	flag.StringVar(&platform, "platform", "cf", "platform to run tests against")
+	flag.StringVar(&stack, "stack", "cflinuxfs3", "stack to use when pushing apps")
 	flag.Parse()
 }
 
