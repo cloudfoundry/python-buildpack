70bd7f607b91f2690a97a71e7cf22ff00df688ab
Klaas van Schelven <klaas@vanschelven.com>
2022-11-08 21:02:47 +0100

    Fix the tests
    there is a small assymmetry between RunPipVendored and RunPipUnvendored's errror
    messages because the latter prepends 'could not run pip'; this became apparant
    in the tests because RunPipVendored no longer calls RunPipUnvendored.
    
    We've simply harmonized on 'could not run pip' and removed a call to pip from the tests
    (we no longer do that)

--- src/python/supply/supply.go
+++ src/python/supply/supply.go
@@ -704,7 +704,7 @@ func (s *Supplier) RunPipVendored() error {
 
 	if err := s.runPipInstall(installArgs...); err != nil {
         s.Log.Info("Running pip install failed. You need to include all dependencies in the vendor directory.")
-        return err
+        return fmt.Errorf("could not run pip: %v", err)
 	}
 
 	return s.Stager.LinkDirectoryInDepDir(filepath.Join(s.Stager.DepDir(), "python", "bin"), "bin")
