8a1e6f305f0fb6920eafa9a7b503758c9a2307e1
ivanovac <ivanovac@gmail.com>
2026-03-12 10:59:46 +0200

    feat: Update install_go.sh to use Go 1.25.7
    - Use cflinuxfs4 Go binary for all stacks (compatible with cflinuxfs5)
    - Remove cflinuxfs3 support (deprecated stack)
    - Single version/SHA simplifies maintenance

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -5,22 +5,23 @@ set -u
 set -o pipefail
 
 function main() {
-  if [[ "${CF_STACK:-}" != "cflinuxfs3" && "${CF_STACK:-}" != "cflinuxfs4" && "${CF_STACK:-}" != "cflinuxfs5" ]]; then
+  if [[ "${CF_STACK:-}" != "cflinuxfs4" && "${CF_STACK:-}" != "cflinuxfs5" ]]; then
     echo "       **ERROR** Unsupported stack"
     echo "                 See https://docs.cloudfoundry.org/devguide/deploy-apps/stacks.html for more info"
     exit 1
   fi
 
   local version expected_sha dir
-  version="1.25.6"
-  expected_sha="0ed64e3b9cb9b1c2ec57880dae2427b0ee2676f2ae2fb53c2e1bb838c500f9fb"
+  version="1.25.7"
+  expected_sha="12a6e116cffdcd071988cf3c30216a3f08f54d2cfbb45fff67e375823fd0c3b9"
   dir="/tmp/go${version}"
 
   mkdir -p "${dir}"
 
   if [[ ! -f "${dir}/bin/go" ]]; then
     local url
-    url="https://buildpacks.cloudfoundry.org/dependencies/go/go_${version}_linux_x64_${CF_STACK}_${expected_sha:0:8}.tgz"
+    # Use cflinuxfs4 binary for all stacks (compatible with cflinuxfs5)
+    url="https://buildpacks.cloudfoundry.org/dependencies/go/go_${version}_linux_x64_cflinuxfs4_${expected_sha:0:8}.tgz"
 
     echo "-----> Download go ${version}"
     curl "${url}" \
